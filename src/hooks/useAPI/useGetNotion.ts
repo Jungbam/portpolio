d6397c33f257aefaf3bacc749fd9ff4654b34fdb
jungbam <jungbam0809@gmail.com>
2023-09-12 22:39:03 +0900

    [FEAT] 이미지 우선순위 부여, 프로젝트 최신순 정렬

--- src/hooks/useAPI/useGetNotion.ts
+++ src/hooks/useAPI/useGetNotion.ts
@@ -4,7 +4,16 @@ import axios from "axios";
 const useGetNotion = () => {
   return useQuery({
     queryKey: ["projects"],
-    queryFn: async () => await axios.get("/api/notion"),
+    queryFn: async () => {
+      const result = await axios.get("/api/notion");
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return result.data.data.sort((a: any, b: any) => {
+        return (
+          b.properties.index.rich_text[0].plain_text -
+          a.properties.index.rich_text[0].plain_text
+        );
+      });
+    },
   });
 };
 
